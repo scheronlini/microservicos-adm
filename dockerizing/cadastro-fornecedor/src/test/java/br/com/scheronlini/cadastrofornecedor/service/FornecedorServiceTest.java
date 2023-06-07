4d6d54e29e9fb5eb1be4934d308e90e3cdc5cfd4
scheronlini <scheronlini@gmail.com>
2023-06-07 02:07:49 -0300

    "atualização dos testes unitarios"

--- dockerizing/cadastro-fornecedor/src/test/java/br/com/scheronlini/cadastrofornecedor/service/FornecedorServiceTest.java
+++ dockerizing/cadastro-fornecedor/src/test/java/br/com/scheronlini/cadastrofornecedor/service/FornecedorServiceTest.java
@@ -4,6 +4,7 @@ import br.com.scheronlini.cadastrofornecedor.model.Endereco;
 import br.com.scheronlini.cadastrofornecedor.model.Fornecedor;
 import br.com.scheronlini.cadastrofornecedor.repository.FornecedorRepository;
 import br.com.scheronlini.cadastrofornecedor.service.FornecedorService;
+import br.com.scheronlini.cadastrofornecedor.service.exceptions.DataBaseException;
 import br.com.scheronlini.cadastrofornecedor.service.exceptions.RegraNegocioException;
 import br.com.scheronlini.cadastrofornecedor.service.exceptions.ResourceNotFoundException;
 import br.com.scheronlini.cadastrofornecedor.service.implementacao.EnderecoServiceImplementacao;
@@ -19,7 +20,9 @@ import org.springframework.boot.test.mock.mockito.SpyBean;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.List;
 import java.util.Optional;
+
 
 @SpringBootTest
 @RunWith(SpringRunner.class)
@@ -28,12 +31,12 @@ public class FornecedorServiceTest {
 
     @SpyBean
     FornecedorServiceImplementacao service;
+
     @MockBean
     FornecedorRepository repository;
 
-
     @Test(expected = Test.None.class)
-    public void deveBuscarUmFornecedorPorId(){
+    public void deveBuscarUmFornecedorPorId() {
         var fornecedor = Fornecedor.builder().id(7l).cnpj("16612533000164").celular("47997277704")
                 .nomeFantasia("casaredo").razaoSocial("casa verde").ramoAtividade("madereira").build();
 
@@ -53,14 +56,14 @@ public class FornecedorServiceTest {
     }
 
     @Test(expected = Test.None.class)
-    public void deveValidarCnpj(){
+    public void deveValidarCnpj() {
         Mockito.when(repository.existsByCnpj(Mockito.anyString())).thenReturn(false);
         service.validaCnpj("53902394000130");
     }
 
 
     @Test(expected = RegraNegocioException.class)
-    public void naoDeveValidarCnpj(){
+    public void naoDeveValidarCnpj() {
 
         Mockito.when(repository.existsByCnpj(Mockito.anyString())).thenReturn(true);
 
@@ -72,12 +75,12 @@ public class FornecedorServiceTest {
 
         Mockito.doNothing().when(service).validaCnpj(Mockito.anyString());
 
-        var fornecedor = Fornecedor.builder().id(7l).cnpj("16612533000164").celular("47997277704")
+        var fornecedor = Fornecedor.builder().id(1l).cnpj("16612533000164").celular("47997277704")
                 .nomeFantasia("casaredo").razaoSocial("casa verde").ramoAtividade("madereira").build();
         var endereco= Endereco.builder()
-                .id(7l)
+                .id(1l)
                 .bairro("ggg")
-                .cep("22222")
+                .cep("89224021")
                 .localidade("8888")
                 .logradouro("fgfd")
                 .pontodeReferencia("gdgdfg")
@@ -95,22 +98,73 @@ public class FornecedorServiceTest {
 
     }
 
-    @Test(expected = RegraNegocioException.class)
-    public void naoDeveInserirFornecedorcomCnpjCadastrado(){
+    @Test(expected = DataBaseException.class)
+    public void naoDeveInserirFornecedorcomCnpjCadastrado() {
+
+        Mockito.doNothing().when(service).validaCnpj(Mockito.anyString());
 
         String cnpj = "16612533000164";
-        var fornecedor = Fornecedor.builder().cnpj(cnpj).celular("47997277704")
+        var fornecedor = Fornecedor.builder().id(1l).cnpj(cnpj).celular("47997277704")
+                .nomeFantasia("casaredo").razaoSocial("casa verde").ramoAtividade("madereira").build();
+        var endereco= Endereco.builder()
+                .id(1l)
+                .bairro("ggg")
+                .cep("89224021")
+                .localidade("8888")
+                .logradouro("fgfd")
+                .pontodeReferencia("gdgdfg")
+                .numero("dfgdg")
+                .uf("fd")
+                .build();
+
+        Mockito.when(repository.save(Mockito.any(Fornecedor.class))).thenReturn(fornecedor);
+        Mockito.when(repository.findById(fornecedor.getId())).thenReturn(Optional.of(fornecedor));
+        Mockito.when(service.insertEndereco(fornecedor)).thenReturn(endereco);
+
+        Fornecedor fornecedorSalvo = service.insert(fornecedor);
+
+
+        Mockito.doThrow(RegraNegocioException.class).when(service).validaCnpj(cnpj);
+
+        var fornecedor2 = Fornecedor.builder().id(1l).cnpj(cnpj).celular("47997277704")
                 .nomeFantasia("casaredo").razaoSocial("casa verde").ramoAtividade("madereira").build();
 
-       Mockito.doThrow(RegraNegocioException.class).when(service).validaCnpj(cnpj);
 
-       service.insert(fornecedor);
+        var endereco2= Endereco.builder()
+                .id(1l)
+                .bairro("ggg")
+                .cep("89224021")
+                .localidade("8888")
+                .logradouro("fgfd")
+                .pontodeReferencia("gdgdfg")
+                .numero("dfgdg")
+                .uf("fd")
+                .build();
 
-       Mockito.verify(repository, Mockito.never()).save(fornecedor);
+        Mockito.when(repository.save(Mockito.any(Fornecedor.class))).thenReturn(fornecedor2);
+        Mockito.when(repository.findById(fornecedor.getId())).thenReturn(Optional.of(fornecedor2));
+        Mockito.when(service.insertEndereco(fornecedor2)).thenReturn(endereco2);
+
+        Fornecedor fornecedorSalvo2 = service.insert(fornecedor2);
+
+        Mockito.verify(repository, Mockito.never()).save(fornecedorSalvo2);
 
     }
 
+    @Test(expected = Test.None.class)
+    public void deveBuscarTodosOsFornecedores() {
+        var fornecedor = Fornecedor.builder().id(1l).cnpj("16612533000164").celular("47997277704")
+                .nomeFantasia("casaredo").razaoSocial("casa verde").ramoAtividade("madereira").build();
 
+        List<Fornecedor> listaFornecedores = List.of(fornecedor);
+        Mockito.when(repository.findAll()).thenReturn(listaFornecedores);
+
+        service.findAll();
+
+        org.junit.jupiter.api.Assertions.assertNotNull(listaFornecedores);
+        org.junit.jupiter.api.Assertions.assertEquals(1, listaFornecedores.size());
+        org.junit.jupiter.api.Assertions.assertEquals(Fornecedor.class, listaFornecedores.get(0).getClass());
+    }
 
 }
 
